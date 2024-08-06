4a90a73894299e1832d19c736678690d8f936ac8
kdt4 <insertvaue@naver.com>
2024-08-06 11:14:32 +0900

    exceptionHandler 처리 완료

--- src/main/java/com/tenco/bank/controller/MainController.java
+++ src/main/java/com/tenco/bank/controller/MainController.java
@@ -1,14 +1,25 @@
 package com.tenco.bank.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.tenco.bank.handler.exception.DataDeliveryException;
+import com.tenco.bank.handler.exception.RedirectException;
+import com.tenco.bank.handler.exception.UnAuthorizedException;
+
+import jakarta.annotation.PostConstruct;
 
 @Controller  // IoC 대상(싱글톤 패턴 관리가 된다.) --> 제어의 역전  
 public class MainController {
 	
 	// REST API  기반으로 주소설계 가능 
-	
+		
+	// 	// http:localhost:8080/main/main-page
 	// 주소설계 
 	// http:localhost:8080/main-page
 	@GetMapping({"/main-page", "/index"})
@@ -19,9 +30,39 @@ public class MainController {
 		// prefix: /WEB-INF/view
 		//         /main  
 		// suffix: .js 
-		return "/main";
+		return "main";
 	}
 	
+	// todo - 삭제 예정 
+	// 주소 설계 
+	// http://localhost:8080/error-test1/true
+	// http://localhost:8080/error-test1/false
+	
+	@GetMapping("/error-test1")
+	public String errorPage() {
+		if(true) {
+			throw new RedirectException("잘못된 요청입니다.", HttpStatus.NOT_FOUND);
+		}
+		return "main";
+	} 
+	
+	
+	// http://localhost:8080/error-test2
+	@GetMapping("/error-test2")
+	public String errorData2() {
+		if(true) {
+			throw new DataDeliveryException("잘못된 데이터 입니다", HttpStatus.BAD_REQUEST);
+		}
+		return "main";
+	} 
+	
+	@GetMapping("/error-test3")
+	public String errorData3() {
+		if(true) {
+			throw new UnAuthorizedException("인증 안된 사용자 입니다.", HttpStatus.UNAUTHORIZED);
+		}
+		return "main";
+	}
 }
 
 
